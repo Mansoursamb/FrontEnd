d32044ba4870b875c58b624206edef968dad06c7
Mansoursamb <sambmansour.ms@gmail.com>
2024-10-21 23:02:39 +0200

    affichage des elements du tableau sur le dom

--- index.js
+++ index.js
@@ -5,6 +5,29 @@ async function getWorks() {
   const url = "http://localhost:5678/api/works";
   const fetcher = await fetch(url);
   const resp = await fetcher.json().then(data);
-  console.log(resp);
+  return resp;
 }
 getWorks();
+
+async function showWorks() {
+  const arrayWorks = await getWorks();
+  console.log(arrayWorks);
+
+  arrayWorks.forEach((work) => {
+    genererWorks(work);
+  });
+}
+showWorks();
+function genererWorks(work) {
+  const figure = document.createElement("figure");
+  const img = document.createElement("img");
+  const figcaption = document.createElement("figcaption");
+  img.src = work.imageUrl;
+  figcaption.textContent = work.title;
+
+  galerie.classList.add("gallery");
+  figure.appendChild(img);
+  figure.appendChild(figcaption);
+  galerie.appendChild(figure);
+}
+genererWorks(work);
